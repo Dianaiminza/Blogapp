4beb8752f2b725c43a0b0b17ff6ad45b12e38013
Dianaiminza <dianaiminza.99@gmail.com>
2021-10-20 18:28:10 +0300

    make it better

---
+++ index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var cors = require('cors');
+var mongoose = require('mongoose');
+var dotenv = require('dotenv');
+
+var postRoute =require ('./routes/posts.js');
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(
+  express.urlencoded({
+    limit: "30mb",
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({
+    author: "Ogulcan",
+    message: "Hello, MERN is awesome!",
+  });
+});
+
+app.use('/api/posts', postRoute);
+
+
+const PORT = process.env.PORT || 8080;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT} `);
+    });
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
+  if(process.env.NODE_ENV ==='production'){
+  app.use(express.static('client/build'));
+  
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+  
+  });
+  }
---
+++ routes/posts.js
@@ -0,0 +1,56 @@
+var Post =require("../models/posts.js");
+var express =require ('express');
+var router=express.Router();
+router.get("/", async (req, res) => {
+  const posts = await Post.find();
+  res.send(posts);
+});
+
+router.get("/:id", async (req, res) => {
+  const { id: _id } = req.params;
+  const post = await Post.findById({ _id });
+  if (post) {
+    res.send(post);
+  } else {
+    res.status(404).send("Blog Not Found.")
+  }
+});
+
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({
+      message: error.message,
+    });
+  }
+});
+
+router.put("/:id",async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(_id, post, { new: true });
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(409).json({
+      message: error.message,
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+ const { id: _id } = req.params;
+  try {
+    const deletedPost = await Post.findByIdAndRemove(_id);
+    res.status(200).json(deletedPost);
+  } catch (error) {
+    res.status(409).json({
+      message: error.message,
+    });
+  }
+});
+
+module.exports=router; 
