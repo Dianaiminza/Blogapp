6781e77804ac855cdf5735dac127c5621365cf78
DIANA IMINZA <51244262+Dianaiminza@users.noreply.github.com>
2021-10-21 18:43:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 module.exports={
  PORT: process.env.PORT || 8080,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://dbUser:Captain@blog.qr81l.mongodb.net/blog?retryWrites=true&w=majority',
+  MONGODB_URL: process.env.MONGODB_URL || '',
   
 
 };
